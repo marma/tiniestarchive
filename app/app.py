9895ef2eb41aa59fffd4915d6ba41ea52a78f65c
Martin Malmsten <martin@martinmalmsten.net>
2025-01-29 08:21:03 +0100

    Removed all UI stuff from app,py

--- app/app.py
+++ app/app.py
@@ -1,19 +1,15 @@
+from tempfile import gettempdir
 from fastapi import FastAPI, Request, HTTPException, File, UploadFile, status
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse,RedirectResponse,JSONResponse,FileResponse,StreamingResponse
-from starlette.requests import ClientDisconnect
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse,JSONResponse,FileResponse,StreamingResponse,PlainTextResponse
 from uuid_utils import uuid7
-from tiniestarchive import FileArchive
-from tiniestarchive.utils import split_path,safe_path,save_to_tmp
-from itertools import islice
+from uuid import UUID, uuid4
+from tiniestarchive import FileArchive,FileInstance,FileResource
 
 from typing import List
 from os import getenv,walk,listdir,makedirs
 from os.path import exists,join,dirname
 import logging
 from json import dumps,loads
-import shutil
 
 ARCHIVE_DIR=getenv('DATA_DIR', '/data')
 LOG_LEVEL=getenv('LOG_LEVEL', 'WARNING')
@@ -21,179 +17,57 @@ PREFIX=getenv('PREFIX', None)
 logging.basicConfig(level=LOG_LEVEL)
 
 app = FastAPI(root_path=PREFIX)
-static = StaticFiles(directory="static")
-templates = Jinja2Templates(directory="templates")
 archive = FileArchive(ARCHIVE_DIR)
 
-def _list_resources(max=None):
-    return islice(archive, max)
-
-def _get_resource(resource_id):
-    return archive.get(resource_id)
-
-def _get_json(resource_id):
-    return archive.json(resource_id)
-
-def _events(start=None, max=None):
-    return islice(archive.events(start=start), max)
-
-@app.get("/{resource_id}/_json", response_class=JSONResponse)
-async def json(resource_id: str):
-    try:
-        return _get_json(resource_id)
-    except FileNotFoundError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Resource not found')
-    except:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Error reading resource')
+@app.get("/{resource_id}/", response_class=JSONResponse)
+async def get_resource(resource_id : UUID):
+    return archive.get(resource_id).json()
 
 @app.get("/{resource_id}/{filename}", response_class=FileResponse)
-async def get_file(resource_id: str, filename: str):
-    if resource_id not in archive:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='File not found')
-
+async def get_file(resource_id : UUID, filename: str):
     return FileResponse(archive._resolve(resource_id, filename))
 
+@app.post("/{resource_id}/_add")
+async def add(resource_id : UUID, files: List[UploadFile]):
+    with archive.get(resource_id, mode='w') as r:
+        with r.transaction() as t:
+            for file in files:
+                t.add(file.filename, data=file.file)
 
-@app.get("/{resource_id}/", response_class=HTMLResponse)
-async def json(resource_id: str, request : Request):
-    if resource_id not in archive:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Resource not found')
+    return "OK"
 
-    if request.headers.get('Accept') == 'application/json':
-        return _get_json(resource_id)
+@app.post("/{resource_id}/_merge")
+async def ingest(resource_id : UUID, file: UploadFile):
+    with FileInstance.deserialize(file.stream) as instance:
+        archive.update(resource_id, instance)
 
-    return templates.TemplateResponse(
-                "resource.html",
-                {
-                    "request": request,
-                    "resource_id": resource_id,
-                    "resource": _get_json(resource_id),
-                })
+@app.get("{resource_id}/_serialize", response_class=StreamingResponse)
+async def stream(resource_id : UUID):
+    return StreamingResponse(
+            archive.serialize(resource_id),
+            media_type='application/tar')
 
-@app.post('/_ingest')
-async def ingest():
-    archive.ingest(request.stream())
+@app.get("/_resources", response_class=PlainTextResponse)
+async def resources(request: Request):
+    # TODO implement paging
+    def i(start=0, max=None):
+        for r in archive:
+            yield r + '\n'
 
-    i = archive.new(mode='w')
+    return StreamingResponse(i, media_type='text/plain')
 
-    print(i)
+@app.get("/_events", response_class=JSONResponse)
+async def events(request: Request):
+    start = request.query_params.get('start', None)
+    max = request.query_params.get('max', None)
 
-    return RedirectResponse(f'/{i.instance_id}/', status_code=status.HTTP_302_FOUND)
-
-@app.get("/_resources", response_class=JSONResponse)
-async def resources():
-    return _list_resources()
-
-@app.get("/")
-async def get_root(request: Request):
-    return templates.TemplateResponse("index.html", { "request": request, "instances": _list_resources(max=100) })
-
-@app.get("/{instance_id}/{filename}", response_class=FileResponse)
-async def get_file(instance_id: str, filename: str):
-    if instance_id not in archive:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='File not found')
-
-    return FileResponse(archive.resolve(instance_id, filename))
-
-@app.post("/{instance_id}/_add", response_class=JSONResponse)
-def add_files(instance_id, files: List[UploadFile]):
-    # TODO: use streaming and validate checksums
-    with archive.get(instance_id, mode='a') as i:
-        for file in files:
-            i.add(file.filename, fobj=file.file)
-
-    return RedirectResponse(f'/{i.instance_id}/', status_code=status.HTTP_302_FOUND)
-
-@app.get("/{instance_id}/")
-async def get_collection(instance_id: str, request: Request):
-    if instance_id not in archive:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Instance not found')
-
-    if request.headers.get('Accept') == 'application/json':
-        return _meta(instance_id)
-
-    return templates.TemplateResponse(
-                "instance.html",
-                {
-                    "request": request,
-                    "instance_id": instance_id,
-                    "meta": _meta(instance_id),
-                    "files": sorted(_list_files(instance_id))
-                })
-
-@app.post("/{instance_id}/_finalize", response_class=JSONResponse)
-async def close_instance(instance_id: str, request: Request):
-    if instance_id not in archive:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Instance not found')
-
-    with archive.get(instance_id, mode='a') as i:
-        if i.config['status'] != 'open':
-            return HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Instance not open')
-
-        i.finalize()
-
-    return { "message": "Instance finalized" }
-
-@app.post("/_events", response_class=JSONResponse)
-async def get_events(request: Request):
-    ...
+    return StreamingResponse(
+            archive.events(
+                start=start,
+                max=max),
+            media_type='text/jsonl')
 
 @app.get('/ok')
 async def ok():
     return "ok"
-
-@app.post('/{instance_id}/_add', response_class=JSONResponse)
-async def add_files(instance_id: str, request: Request):
-    if not instance_id in archive:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Instance not found')
-
-    if _meta(instance_id)['status'] == 'finalized':
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Instance is finalized')
-
-    # TODO: lock instance
-    # TODO: check version
-
-    tmpdir = join(_resolve(instance_id), f'.tmp-{str(uuid7())}')
-    touched=False
-    try:
-        filenames, checksums = await save_to_tmp(instance_id, tmpdir, request)
-
-        # check if all files exists in tmpdir and validate
-        for filename in filenames:
-            if not exists(join(tmpdir, filename)):
-                raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail='File {filename} not found in request')
-            
-            # TODO: validate file checksum against supplied checksum
-
-        # move files to final location
-        for filename in filenames:
-            target_path = _resolve(instance_id, filename)
-
-            if not exists(dirname(target_path)):
-                makedirs(dirname(target_path))
-
-            # move within the same filesystem assumed to be atomic
-            shutil.move(join(tmpdir, filename), target_path)
-            touched=True
-            
-        # TODO: (optionally) validate files in final location
-    except ClientDisconnect:
-        logging.warning("Client Disconnected")
-    except HTTPException as e:
-        logging.warning(e)
-        raise e
-    except Exception as e:
-        logging.exception(e)
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
-            detail='There was an error uploading the file(s)')
-    finally:
-        shutil.rmtree(tmpdir, ignore_errors=True)
-
-        if touched:
-            _touch(instance_id)
-
-    logging.info(f"filenames: {filenames}, checksums: {checksums}")
-
-    return { "files": checksums }
 
